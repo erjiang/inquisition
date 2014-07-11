6afc2181c1102e34655184af6ac9c61a9e7a1b2d
Eric Jiang <eric@doublemap.com>
2014-07-10 23:14:48 -0400

    Make every Type inherit from Type and have accepts() method

--- pypes.py
+++ pypes.py
@@ -1,4 +1,13 @@
-class FuncType():
+class Type():
+    def accepts(self, T):
+        """
+        Returns true if this the type T is equal to this type or is a subset of
+        this type.
+        """
+        return T == self
+
+
+class FuncType(Type):
     args = []
     kwargs = []
     ret = None
@@ -13,13 +22,30 @@ class FuncType():
         args = ", ".join(list(map(str, self.args)) + self.kwargs)
         return "(%s) -> %s" % (args, str(self.ret))
 
+    def accepts(self, T):
+        if not isinstance(T, FuncType):
+            return False
+        if len(T.args) != len(self.args):
+            return False
+        if not type_fits(T.ret, self.ret):
+            return False
+        # check that all the args match
+        # TODO: kwargs
+        return all([type_fits(A, B) for A, B in zip(self.args, T.args)])
 
-class AnyType():
+
+class AnyType(Type):
     def __init__(self, magic_words=None):
         if magic_words != "nuh-uh-uh":
             raise Exception("Shouldn't instantiate AnyType")
     def __str__(self):
         return "?"
+
+    def accepts(self, T):
+        """
+        AnyType can be anything!
+        """
+        return True
 
 
 unknown = AnyType("nuh-uh-uh")
@@ -33,6 +59,9 @@ class Overload(set): # :: set(FuncType)
                 return overload.ret
         raise Heresy("No.")
 
+    def accepts(self, T):
+        return any([type_fits(T, X) for X in self])
+
 
 def type_fits(A, B):
     """
@@ -43,19 +72,15 @@ def type_fits(A, B):
       * B is SomeType and A is in B
       * B is Maybe(X) and A is None or X
     """
-    if A == B:
+    if A == B:  # if B is a str or A==B
         return True
     elif B == unknown:
         return True
-    elif isinstance(B, SomeType):
-        return A in B
-    elif isinstance(B, Maybe):
-        return A is None or type_fits(A, B.concrete)
-    else:
-        return False
+    else:  # assume that B is a Type sub-class
+        return B.accepts(A)
 
 
-class Maybe():
+class Maybe(Type):
     """
     Represents a Nunnable type - a type that can be either the concrete type,
     or None. For example, Maybe(int) may be int or may be None.
@@ -65,6 +90,9 @@ class Maybe():
         if concrete is None:
             raise ValueError("Can't have Maybe(None) - doesn't make sense")
         self.concrete = concrete
+
+    def accepts(self, T):
+        return T is None or type_fits(T, self.concrete)
 
 
 SomeType = set
